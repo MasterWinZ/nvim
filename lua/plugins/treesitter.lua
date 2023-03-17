73234ff2ef8c322a3dae93b04f60889ef6fd9ca9
SP.Win <supawit.promma6256@gmail.com>
2023-03-17 22:45:56 +0700

    feat: add treesitter

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,22 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    event = { "BufReadPost", "BufNewFile" },
+    opts = {
+        sync_install = false,
+	auto_install = true,
+	highlight = {
+		enable = true,
+	},
+	autotag = {
+    	enable = true,
+	}
+    },
+    config = function(_, opts)
+        if vim.fn.has('win32') == 1 then
+            require('nvim-treesitter.install').compilers = { "clang", "gcc" }
+        end
+        require("nvim-treesitter.configs").setup(opts)
+    end
+}
+
