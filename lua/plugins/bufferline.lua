6b967f09e12be15672dda38fd3c17515f65cc722
SP.Win <supawit.promma6256@gmail.com>
2023-03-18 00:08:44 +0700

    feat: change separator style

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -3,7 +3,7 @@ return {
     opts = {
         options = {
             mode = "tabs",
-            separator_style = 'slant',
+            separator_style = 'slope',
             always_show_bufferline = false,
             show_buffer_close_icons = false,
             show_close_icon = false,
