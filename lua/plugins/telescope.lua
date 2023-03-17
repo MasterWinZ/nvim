a6aa209bff7c2b15f55cede54d1f35df638aaa2f
SP.Win <supawit.promma6256@gmail.com>
2023-03-17 23:35:43 +0700

    feat: add telescope

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,28 @@
+return {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "BurntSushi/ripgrep",
+        "nvim-telescope/telescope-file-browser.nvim",
+        "nvim-telescope/telescope-media-files.nvim",
+    },
+    -- using opts created too much unnecessary problems
+    config = function()
+        require('telescope').setup({
+            defaults = {
+                mappings = {
+                    n = {
+                        ['q'] = require('telescope.actions').close
+                    }
+                }
+            },
+            extensions = {
+                file_browser = {
+                    theme = 'dropdown',
+                    hijack_netrw = true,
+                },
+            } 
+        })
+        require("telescope").load_extension('file_browser', 'nvim-telescope/telescope-media-files.nvim')
+    end
+}
--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -6,36 +6,42 @@ vim.g.mapleader = ' '
 map('i', 'jk', '<ESC>')
 
 -- Move to window using the <ctrl> hjkl keys
-map("n", "<C-h>", "<C-w>h")
-map("n", "<C-j>", "<C-w>j")
-map("n", "<C-k>", "<C-w>k")
-map("n", "<C-l>", "<C-w>l")
+map('n', '<C-h>', '<C-w>h')
+map('n', '<C-j>', '<C-w>j')
+map('n', '<C-k>', '<C-w>k')
+map('n', '<C-l>', '<C-w>l')
 
 -- Split window
-map("n", "<leader>sp", "<C-w>s")
-map("n", "<leader>vs", "<C-w>v")
+map('n', '<leader>sp', '<C-w>s')
+map('n', '<leader>vs', '<C-w>v')
 
 -- Capitalize
-map("n", "gu", "~h")
-map("x", "gu", "~")
+map('n', 'gu', '~h')
+map('x', 'gu', '~')
 
 -- <ESC> and jk to exit insert mode in terminal
-map("t", "<ESC>", "<C-\\><C-n>")
+map('t', '<ESC>', '<C-\\><C-n>')
 
 -- Resize window using <ctrl> arrow keys
-map("n", "<C-Up>", "<cmd>resize +2<cr>")
-map("n", "<C-Down>", "<cmd>resize -2<cr>")
-map("n", "<C-Right>", "<cmd>vertical resize -2<cr>")
-map("n", "<C-Left>", "<cmd>vertical resize +2<cr>")
+map('n', '<C-Up>', '<CMD>resize +2<CR>')
+map('n', '<C-Down>', '<CMD>resize -2<CR>')
+map('n', '<C-Right>', '<CMD>vertical resize -2<CR>')
+map('n', '<C-Left>', '<CMD>vertical resize +2<CR>')
 
 -- Move Lines
-map("n", "<A-j>", "<cmd>m .+1<cr>==")
-map("n", "<A-k>", "<cmd>m .-2<cr>==")
-map("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi")
-map("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi")
-map("v", "<A-j>", ":m '>+1<cr>gv=gv")
-map("v", "<A-k>", ":m '<-2<cr>gv=gv")
+map('n', '<A-j>', '<CMD>m .+1<cr>==')
+map('n', '<A-k>', '<CMD>m .-2<cr>==')
+map('i', '<A-j>', '<ESC><cmd>m .+1<cr>==gi')
+map('i', '<A-k>', '<ESC><cmd>m .-2<cr>==gi')
+map('v', '<A-j>', '<CMD>m ">+1<cr>gv=gv')
+map('v', '<A-k>', '<CMD>m "<-2<cr>gv=gv')
 
--- better indenting
-map("v", "<", "<gv")
-map("v", ">", ">gv")
+-- Better indenting
+map('v', '<', '<gv')
+map('v', '>', '>gv')
+
+-- Telescope
+map('n', '<leader>fe', '<CMD>Telescope file_browser<CR>')
+map('n', '<leader>ff', '<CMD>Telescope find_files<CR>')
+map('n', '<leader>fh', '<CMD>Telescope help_tags<CR>')
+map('n', '<leader>fg', '<CMD>Telescope live_grep<CR>')
