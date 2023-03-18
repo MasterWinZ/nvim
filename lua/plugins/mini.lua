317b42c59a437dbe749b23f6ac2eda33de9fd43a
SP.Win <supawit.promma6256@gmail.com>
2023-03-18 15:48:21 +0700

    feat: add mini surround, pair, comment

---
+++ lua/plugins/mini.lua
@@ -0,0 +1,22 @@
+return {
+    {
+        'echasnovski/mini.comment',
+        config = function()
+            require('mini.comment').setup()
+        end
+    },
+
+    {
+        'echasnovski/mini.surround',
+        config = function()
+            require('mini.surround').setup()
+        end
+    },
+
+    {
+        'echasnovski/mini.pairs',
+        config = function()
+            require('mini.pairs').setup()
+        end
+    },
+}
