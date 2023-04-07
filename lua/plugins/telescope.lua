4c83626390fa42efbf14d10ff64d2a348152730a
SP.Win <supawit.promma6256@gmail.com>
2023-04-07 21:03:08 +0700

    feat: change all used telescope theme to dropdown

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -11,16 +11,63 @@ return {
             defaults = {
                 mappings = {
                     n = {
-                        ['q'] = require('telescope.actions').close
+                        ['q'] = require('telescope.actions').close,
+                        ['sh'] = require('telescope.actions').select_horizontal,
+			['sv'] = require('telescope.actions').select_vertical,
+			['te'] = require('telescope.actions').select_tab,
                     }
                 }
+            },
+            pickers = {
+                find_files = {
+                    hidden = true,
+                    theme = 'dropdown',
+                    previewer = false,
+                    layout_config = { height = 20 }
+                },
+
+                help_tags = {
+                    theme = 'dropdown',
+                    previewer = false,
+                    layout_config = { height = 20 }
+                },
+
+                buffers = {
+                    hidden = true,
+                    theme = 'dropdown',
+                    initial_mode = 'normal',
+                    previewer = false,
+                    layout_config = { height = 20 }
+                },
+
+                live_grep = {
+                    hidden = true,
+                    theme = 'dropdown',
+                    previewer = false,
+                    layout_config = { height = 20 }
+                },
+
+                diagnostics = {
+                    hidden = true,
+                    theme = 'dropdown',
+                    initial_mode = 'normal',
+                    previewer = false,
+                    layout_config = { height = 20 }
+                },
             },
             extensions = {
                 file_browser = {
                     theme = 'dropdown',
                     hijack_netrw = true,
+                    path = "%:p:h",
+                    respect_gitignore = false,
+                    hidden = true,
+                    grouped = true,
+                    initial_mode = 'normal',
+                    previewer = false,
+                    layout_config = { height = 20 }
                 },
-            } 
+            }
         })
         require('telescope').load_extension('file_browser', 'nvim-telescope/telescope-media-files.nvim')
     end
